8c54e2644ca7993d1b3566f75d31a32a332a26f4
Luke Gilson <luke.gilson@gmail.com>
2016-10-21 12:58:15 -0400

    updated options and docs

--- parse_location_history/parse_locations.py
+++ parse_location_history/parse_locations.py
@@ -8,7 +8,12 @@ def file_to_dict(json_file):
 
 def lat_long_diff(lat_long_1, lat_long_2):
     """ Diff in meters between two points
-        for spherical triangle ABC where BC is what we want...
+        for spherical triangle ABC where BC is what we want
+
+        for reference, see :
+        http://mathworld.wolfram.com/SphericalTrigonometry.html
+        http://mathworld.wolfram.com/Arc.html
+        https://en.wikipedia.org/wiki/Spherical_law_of_cosines
     """
 
     # radians for arc AB
@@ -29,6 +34,16 @@ def timestamp_to_date(timestamp):
     return datetime.datetime.strftime(d, "%Y-%m-%d")
 
 def update_farthest(location_obj, calendar_obj, home_coords):
+    """ Return updated version of calendar_obj
+
+        location_obj - @param - dict including key-vals for timestampMs, latitudeE7, longitudeE7
+                     - @type - dict
+        calendar_obj - @param - {"date" : dist, "date" : dist, ...}
+                     - @type - dict
+        home_coords - @param - [lat, long] to find distance from
+                    - @type - list
+    """
+
     d = timestamp_to_date(location_obj["timestampMs"])
     away_coords = [location_obj["latitudeE7"] / 10000000.0, location_obj["longitudeE7"] / 10000000.0]
     dist = lat_long_diff(home_coords, away_coords)
@@ -41,8 +56,19 @@ def update_farthest(location_obj, calendar_obj, home_coords):
     return calendar_obj
 
 def process_file(start_date, end_date, file_path, home_coords):
+    """ Return {date : dist, date : dist, ...}
+
+        start_date - @param - the YYYY-MM-DD to begin search the file, inclusive
+                   - @type - str
+        end_date - @param - the YYYY-MM-DD to end searching the file, exclusive
+                 - @type - str
+        file_path - @param - the path of the source json file
+                  - @type - str
+        home_coords - @param - [lat, long] in decimal notation (e.g. 40.4, not 40deg 30min)
+                    - @type - list
+    """
     json_data = file_to_dict(file_path)
-    
+
     start_ts = time.mktime(start_date.timetuple())
     end_ts = time.mktime(end_date.timetuple())
     calendar_obj = {}
@@ -55,12 +81,12 @@ def process_file(start_date, end_date, file_path, home_coords):
         if (ts >= start_ts and ts < end_ts):
             j += 1
             calendar_obj = update_farthest(location_obj, calendar_obj, home_coords)
-        
+
     return calendar_obj
 
 def insert_data(tbl, new_item, sort_key):
     """ tbl is a multidimensional list """
-    
+
     tl = len(tbl)
     if (tl == 0):
         return [new_item]
@@ -68,21 +94,25 @@ def insert_data(tbl, new_item, sort_key):
     for i in range(tl):
 
         if (new_item[sort_key] <= tbl[i][sort_key]):
-            
+
             nt = tbl[:i] + [new_item] + tbl[i:]
             return tbl[:i] + [new_item] + tbl[i:]
-        
+
     tbl.append(new_item)
     return tbl
 
-def dict_to_tbl(obj, col):
-    """ turn dict to tbl, sort by col number """
+def sort_data(obj, col):
+    """ sort data by col number. Returns multidim. list """
     nt = []
     for key in obj:
         nt = insert_data(nt, [key, obj[key]], col)
+    return nt
+
+def convert_to_tsv(obj):
+    """ turn list to tsv tbl """
 
     fs = ""
-    for i in nt:
+    for i in obj:
         fs += "{}\n".format("\t".join(map(str, i)))
     return fs
 
@@ -97,9 +127,12 @@ if __name__ == "__main__":
     parser.add_argument("-u","--longitude", help="longitude of home address (in decimal degrees)", required=True, type=float)
     parser.add_argument("-l","--latitude", help="latitude of home address (in decimal degrees)", required=True, type=float)
     parser.add_argument("-d","--distanceorder", help="order by distance ascending", required=False, action="store_true")
+    parser.add_argument("-t","--format", help="`tsv` or `array`, a multidimensional array string", default="array", required=True)
+    parser.add_argument("-o","--out", help="file to write to. If none, writes to stdout", required=False)
 
     args = parser.parse_args()
 
+    # Parse date range. Defaults to epoch until today
     if (args.start):
         start_time = datetime.datetime.strptime(args.start, "%Y-%m-%d")
     else:
@@ -109,16 +142,32 @@ if __name__ == "__main__":
     else:
         end_time = datetime.datetime.today()
 
+    # Get path of the file supplied by --file.
     json_file = os.path.abspath(os.path.expanduser(args.file))
     if (not os.path.isfile(json_file)):
         raise ValueError("Specified file {} does not exist when expanded as {}".format(args.file, json_file))
 
     home_coords = [args.latitude, args.longitude]
+
+    # read through the file, returning an unsorted list [[date, dist], [date, dist]]
     result = process_file(start_time, end_time, json_file, home_coords)
 
-    print "done with process_file"
-
     if (args.distanceorder):
-        print dict_to_tbl(result, 1)
+        result = sort_data(result, 1)
     else:
-        print dict_to_tbl(result, 0)
+        result = sort_data(result, 0)
+
+    if (args.format == "tsv"):
+        result = convert_to_tsv(result)
+
+
+    if (args.out):
+        f = os.path.abspath(os.path.expanduser(args.out))
+        d = os.path.dirname(f)
+        if (not os.path.isdir(d)):
+            print "the directory {} does not exist, writing to stdout".format(d)
+            print result
+        f = open(f, "w")
+        f.write(result)
+    else:
+        print result
